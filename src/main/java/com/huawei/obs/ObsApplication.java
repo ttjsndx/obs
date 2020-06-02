d4e367fe27cf081672a83c3a76b3e32b12b7598a
ttjsndx <243017037@qq.com>
2020-06-02 19:57:47 +0800

    项目初始化

---
+++ src/main/java/com/huawei/obs/ObsApplication.java
@@ -0,0 +1,13 @@
+package com.huawei.obs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObsApplication.class, args);
+	}
+
+}
